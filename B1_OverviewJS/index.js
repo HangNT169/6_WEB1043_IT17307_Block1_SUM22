629227e131b2f664819a36b9cd56fd190a8895c1
HangNT169 <hN160499aA@>
2022-05-10 13:27:14 +0700

    B1_OverviewJS

---
+++ B1_OverviewJS/index.js
@@ -0,0 +1,18 @@
+document.write("JS ngoai file HTML");
+alert("Thong bao");
+console.log("log thong bao");
+// var number = 5;
+// console.log(number);
+// var number = prompt("Nhap:");
+// console.log(number);
+// var number = 6;
+// if (number % 2 == 0) {
+//   console.log("chan");
+// } else {
+//   console.log("le");
+// }
+
+var numberFirst = 100;
+var numberSecond = 20;
+var total = numberFirst + numberSecond;
+console.log(total);
