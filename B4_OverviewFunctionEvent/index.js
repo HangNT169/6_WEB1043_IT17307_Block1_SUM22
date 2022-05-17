c40dcfef466f2453e5a5ce501bd2d169d38ad693
HangNT169 <hN160499aA@>
2022-05-17 12:51:36 +0700

    B4_OverviewFunction_Event

---
+++ B4_OverviewFunctionEvent/index.js
@@ -0,0 +1,48 @@
+/**
+ * Function(Ham):
+ * - Ham co kieu tra ve
+ * - Ham khong co kieu tra ve
+ */
+
+// Cu phap
+/**
+ * function tenHam(thamSoTruyenVao){
+ *  // code
+ * }
+ */
+
+// // VD:
+// function hienThiThongBao(a) {
+//   console.log(a);
+// }
+// // goi ham
+// hienThiThongBao("Da goi ham thanh cong");
+
+// // Co kieu du lieu tra ve
+// function tongHaiSo(a, b, c) {
+//   return a + b + c;
+// }
+
+// // cach goi
+// let ketQua = tongHaiSo(3, 4, 5);
+// alert(ketQua);
+
+// https://codelearn.io/sharing/function-javascript
+
+// Pham vi truy cap
+/**
+ * 1. Bien toan cuc: goi o moi noi
+ * 2. Bien cuc bo: chi duoc tao ra, goi trong khoi {}
+ */
+// let t1 = "Bien toan cuc";
+// for (let i = 0; i < 3; i++) {
+//   let t2 = "Bien cuc bo";
+//   console.log("Trong vong lap:" + t1);
+//   console.log("Trong vong lap:" + t2);
+// }
+// console.log("Ngoai vong lap:" + t1);
+// console.log("Ngoai vong lap:" + t2);
+// 3. Event
+function clickMe(a) {
+  alert(a);
+}
---
+++ B4_OverviewFunctionEvent/index1.js
@@ -0,0 +1,1 @@
+// hienThiThongBao("Goi Ham Ngoai");
